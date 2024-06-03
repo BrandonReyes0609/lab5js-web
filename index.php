5003665a6ccdefda68b11d841a4d9e1ebba5961f
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-03 12:49:38 -0600

    1

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Redirigir a html/index.html
+header("Location: html/index.html");
+exit(); // Asegúrate de salir después de la redirección para evitar que el script continúe ejecutándose
+?>
